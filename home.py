391fa67a2114bc3c2bc5f5c31367995f3a92156c
Nobiscumdeus <adeolaolumide99@gmail.com>
2025-04-20 03:59:32 +0100

    updated command

--- home.py
+++ home.py
@@ -321,4 +321,4 @@ async def health_check():
 
 
 if __name__ == "__main__":
-    uvicorn.run("home:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("home:app", host="0.0.0.0", port=8000)
